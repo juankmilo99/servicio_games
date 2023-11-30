cea688100180640be0e60b0a1fb876f48dfa1a7a
juankmilo99 <juan.rodriguezs@usantoto.edu.co>
2023-11-29 22:16:55 -0500

    comi

--- src/keys.ts
+++ src/keys.ts
@@ -3,8 +3,8 @@ export default {
     database: {
         dialec: process.env.DIALECT,
         host: process.env.HOST,
-        user: process.env.USER,
-        password: process.env.PASSWORD,
+        user: 'camilo_r',
+        password: 'riO6feEyzGNUR3Vk499tl0tweHlVZEvR',
         database: process.env.DATABASE,
         ssl: Boolean(process.env.SSL)
     }
